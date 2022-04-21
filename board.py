9e26955bcfdebdc77d68bb10248736258643e25f
robpuryear <sdpuryea@us.ibm.com>
2022-04-21 19:45:28 -0400

    Alternate between players with error checking

--- board.py
+++ board.py
@@ -3,13 +3,14 @@ board = [['-', '-', '-'],
          ['-', '-', '-'],
          ['-', '-', '-']]
 
+winner = 'n'
+
 
 def main():
     print_board()
-    player1()
-    print_board()
-    player2()
-    print_board()
+    while winner != 'y':
+        player1()
+        player2()
 
 
 def print_board():
@@ -21,18 +22,23 @@ def print_board():
                 board[r][c] = board[r][c]
         # print board
         print(board[r])
+    print('')
+
+
+def check_for_winner():
+    print("check for winner after each player's move")
 
 
 def player1():
-    print('Player 1, make your move')
-    p1_row_one = int(input('Enter row nos (0-2):'))
-    p1_col_one = int(input('Enter col nos (0-2):'))
+    while True:
+        print('Player 1, make your move')
+        p1_row_one = int(input('Enter row nos (0-2):'))
+        p1_col_one = int(input('Enter col nos (0-2):'))
 
-    # Make sure you only select 0, 1, or 2 or else error:
-    # **** Invalid row or column. Please select row / col between values 0 to 2 ****
-    # Make sure its an valid space to make a move or else error:
-    # Board[1][1] has already been selected. Please choose somewhere else on the board ****
-    # **** Invalid choice. Please mark again! ****
+        if 0 <= p1_row_one <= 2 and 0 <= p1_col_one <= 2:
+            break
+        else:
+            print("**** Invalid row or column. Please select row / col between values 0 to 2 ****")
 
     if board[p1_row_one][p1_col_one] == '-':
         board[p1_row_one][p1_col_one] = 'X'
@@ -43,27 +49,32 @@ def player1():
         print("**** Invalid choice. Please mark again! ****")
         player1()
 
+    print_board()
+    check_for_winner()
+
 
 def player2():
-    print('Player 2, make your move')
-    p2_row_one = int(input('Enter row nos (0-2):'))
-    p2_col_one = int(input('Enter col nos (0-2):'))
+    while True:
+        print('Player 2, make your move')
+        p2_row_one = int(input('Enter row nos (0-2):'))
+        p2_col_one = int(input('Enter col nos (0-2):'))
 
-    # Make sure you only select 0, 1, or 2 or else error:
-    # **** Invalid row or column. Please select row / col between values 0 to 2 ****
-    # Make sure its an valid space to make a move or else error:
-    # Board[1][1] has already been selected. Please choose somewhere else on the board ****
-    # **** Invalid choice. Please mark again! ****
+        if 0 <= p2_row_one <= 2 and 0 <= p2_col_one <= 2:
+            break
+        else:
+            print("**** Invalid row or column. Please select row / col between values 0 to 2 ****")
 
     if board[p2_row_one][p2_col_one] == '-':
         board[p2_row_one][p2_col_one] = 'O'
         print("Player 2 added mark at the location " + str(p2_row_one) + "," + str(p2_col_one))
-
     else:
         print("**** Board [" + str(p2_row_one) + "][" + str(
             p2_col_one) + "] has already been selected. Please choose somewhere else on the board ****")
         print("**** Invalid choice. Please mark again! ****")
         player2()
+
+    print_board()
+    check_for_winner()
 
 
 main()
